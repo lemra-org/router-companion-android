22a6365bc0bd571f35930b6f236e6b4523e493f9
Armel Soro <armel@rm3l.org>
2014-08-11 00:14:23 +0200

    [Dev] Add remaining fragment tabs sections [Dev] Add ACRA lib for crash reporting

---
+++ app/src/main/java/org/lemra/dd_wrt/fragments/AbstractDDWRTBaseFragment.java
@@ -0,0 +1,9 @@
+package org.lemra.dd_wrt.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by armel on 8/10/14.
+ */
+public abstract class AbstractDDWRTBaseFragment extends Fragment {
+}
