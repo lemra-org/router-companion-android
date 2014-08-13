68ef3180bc6e6082b8fe8ed68126916e9d5eca84
Armel Soro <armel@rm3l.org>
2014-08-13 15:28:29 +0200

    [Enhancement] Display "No Data" if concrete fragment implementations return no view

--- app/src/main/java/org/lemra/dd_wrt/fragments/status/StatusWANFragment.java
+++ app/src/main/java/org/lemra/dd_wrt/fragments/status/StatusWANFragment.java
@@ -1,9 +1,20 @@
 package org.lemra.dd_wrt.fragments.status;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.lemra.dd_wrt.fragments.DDWRTBaseFragment;
 
 /**
  * Created by armel on 8/10/14.
  */
 public class StatusWANFragment extends DDWRTBaseFragment {
+    @Nullable
+    @Override
+    protected View getDDWRTSectionView(@NotNull Context context, @NotNull Bundle arguments) {
+        return null;
+    }
 }
