8d346d4876f244ed7dada02c26805f5860f451e9
Armel Soro <armel@rm3l.org>
2014-11-04 11:44:59 +0100

    [Dev] Start implementing Bandwidth Monitoring tiles

--- app/src/main/java/org/lemra/dd_wrt/fragments/status/StatusSyslogFragment.java
+++ app/src/main/java/org/lemra/dd_wrt/fragments/status/StatusSyslogFragment.java
@@ -2,10 +2,14 @@ package org.lemra.dd_wrt.fragments.status;
 
 import android.os.Bundle;
 
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+
 import org.jetbrains.annotations.Nullable;
 import org.lemra.dd_wrt.fragments.DDWRTBaseFragment;
 import org.lemra.dd_wrt.tiles.DDWRTTile;
+import org.lemra.dd_wrt.tiles.syslog.StatusSyslogTile;
 
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -16,6 +20,8 @@ public class StatusSyslogFragment extends DDWRTBaseFragment {
     @Nullable
     @Override
     protected List<DDWRTTile> getTiles(@Nullable Bundle savedInstanceState) {
-        return null;
+        final SherlockFragmentActivity sherlockActivity = getSherlockActivity();
+        return Arrays.<DDWRTTile>asList(
+                new StatusSyslogTile(sherlockActivity, savedInstanceState, this.router));
     }
 }
---
+++ app/src/main/java/org/lemra/dd_wrt/tiles/syslog/StatusSyslogTile.java
@@ -0,0 +1,45 @@
+package org.lemra.dd_wrt.tiles.syslog;
+
+import android.os.Bundle;
+import android.support.v4.content.Loader;
+import android.view.View;
+import android.widget.Toast;
+
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+
+import org.lemra.dd_wrt.R;
+import org.lemra.dd_wrt.api.conn.NVRAMInfo;
+import org.lemra.dd_wrt.api.conn.Router;
+import org.lemra.dd_wrt.tiles.DDWRTTile;
+
+/**
+ * Created by armel on 9/9/14.
+ */
+public class StatusSyslogTile extends DDWRTTile<NVRAMInfo> {
+
+    private static final String LOG_TAG = StatusSyslogTile.class.getSimpleName();
+
+    public StatusSyslogTile(SherlockFragmentActivity parentFragmentActivity, Bundle arguments, Router router) {
+        super(parentFragmentActivity, arguments, router, R.layout.tile_status_router_syslog, R.id.tile_status_router_syslog_togglebutton);
+    }
+
+    @Override
+    protected Loader<NVRAMInfo> getLoader(int id, Bundle args) {
+        return null;
+    }
+
+    @Override
+    protected String getLogTag() {
+        return LOG_TAG;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<NVRAMInfo> loader, NVRAMInfo nvramInfo) {
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        Toast.makeText(this.mParentFragmentActivity, this.getClass().getSimpleName(), Toast.LENGTH_SHORT).show();
+    }
+}
