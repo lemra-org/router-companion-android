eaaf05a43d38685a9cbe2459b231f8708e88afa3
Armel Soro <armel@rm3l.org>
2015-01-01 12:08:20 +0100

    [Fix] Drop duplicate line

--- app/src/main/java/org/rm3l/ddwrt/mgmt/RouterManagementActivity.java
+++ app/src/main/java/org/rm3l/ddwrt/mgmt/RouterManagementActivity.java
@@ -113,8 +113,6 @@ public class RouterManagementActivity
 
         // use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
-        mRecyclerView.setHasFixedSize(true);
-
         // allows for optimizations if all items are of the same size:
         mRecyclerView.setHasFixedSize(true);
 
