9977d44ab22f52184b571fe98e9c0f137aef2b1e
Armel Soro <armel@rm3l.org>
2015-08-16 14:11:33 +0200

    [enhancement] Insert banner ads in between tiles, to optimize impressions

---
+++ app/src/main/java/org/rm3l/ddwrt/tiles/BannerAdTile.java
@@ -0,0 +1,72 @@
+package org.rm3l.ddwrt.tiles;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
+
+import com.google.android.gms.ads.AdView;
+
+import org.rm3l.ddwrt.R;
+import org.rm3l.ddwrt.resources.conn.Router;
+import org.rm3l.ddwrt.utils.AdUtils;
+
+/**
+ * Created by rm3l on 16/08/15.
+ */
+public class BannerAdTile extends DDWRTTile<Void> {
+
+    public BannerAdTile(@NonNull Fragment parentFragment,
+                        @NonNull Bundle arguments,
+                        @Nullable Router router) {
+        super(parentFragment, arguments, router, R.layout.tile_adview, null);
+    }
+
+    @Override
+    public int getTileHeaderViewId() {
+        return -1;
+    }
+
+    @Override
+    public int getTileTitleViewId() {
+        return -1;
+    }
+
+    @Override
+    @Nullable
+    public Integer getTileBackgroundColor() {
+        return mParentFragmentActivity.getResources().getColor(android.R.color.transparent);
+    }
+
+    @Nullable
+    @Override
+    protected Loader<Void> getLoader(int id, Bundle args) {
+        return new AsyncTaskLoader<Void>(mParentFragmentActivity) {
+            @Override
+            public Void loadInBackground() {
+                //Nothing to do
+                return null;
+            }
+        };
+    }
+
+    @Nullable
+    @Override
+    protected String getLogTag() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    protected OnClickIntent getOnclickIntent() {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Void> loader, Void data) {
+        AdUtils.buildAndDisplayAdViewIfNeeded(mParentFragmentActivity,
+                (AdView) layout.findViewById(R.id.router_main_activity_tile_adView));
+    }
+}
