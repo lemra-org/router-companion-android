0eee23c78071ecf3b4b0a60420a2ff953cf52447
Armel Soro <armel@rm3l.org>
2015-11-24 22:12:44 +0100

    [enhancement] Set horizontal progress bar thickness

--- app/src/main/java/org/rm3l/ddwrt/tiles/DDWRTTile.java
+++ app/src/main/java/org/rm3l/ddwrt/tiles/DDWRTTile.java
@@ -493,7 +493,7 @@ public abstract class DDWRTTile<T>
                 if (viewById instanceof ProgressBar) {
                     progressBarViewSeparator = (ProgressBar) viewById;
                     progressBarViewSeparator.setMax(100);
-                    progressBarViewSeparator.setScaleY(0.17f);
+                    progressBarViewSeparator.setScaleY(0.11f);
                 }
             }
             if (progressBarViewSeparator == null) {
