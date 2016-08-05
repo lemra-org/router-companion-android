0cecc2d4979f979d7e4574cf290ca3e96696ad7e
Armel Soro <armel@rm3l.org>
2016-08-06 00:53:51 +0200

    [Tasker] Paving the way for the Tasker Plugin: working example - to be continued

---
+++ tasker-plugin/src/main/java/org/rm3l/ddwrt/tasker/bundle/PluginBundleValues.java
@@ -0,0 +1,7 @@
+package org.rm3l.ddwrt.tasker.bundle;
+
+/**
+ * Created by rm3l on 06/08/16.
+ */
+public class PluginBundleValues {
+}
---
+++ tasker-plugin/src/main/java/org/rm3l/ddwrt/tasker/receiver/Constants.java
@@ -0,0 +1,7 @@
+package org.rm3l.ddwrt.tasker.receiver;
+
+/**
+ * Created by rm3l on 05/08/16.
+ */
+public class Constants {
+}
