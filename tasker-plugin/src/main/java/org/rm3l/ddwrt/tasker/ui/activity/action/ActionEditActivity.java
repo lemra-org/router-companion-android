196dc1d605fe03bd8e861db4ea6d66e3951edc14
Armel Soro <armel@rm3l.org>
2016-09-07 05:44:32 -0400

    [Tasker] Fix AboutLibraries auto-detection

--- tasker-plugin/src/main/java/org/rm3l/ddwrt/tasker/ui/activity/action/ActionEditActivity.java
+++ tasker-plugin/src/main/java/org/rm3l/ddwrt/tasker/ui/activity/action/ActionEditActivity.java
@@ -352,6 +352,7 @@ public class ActionEditActivity extends AbstractAppCompatPluginActivity {
                 break;
             case R.id.ddwrt_companion_tasker_about:
                 new LibsBuilder()
+                        .withFields(R.string.class.getFields())
                         .withActivityTitle("About")
                         //provide a style (optional) (LIGHT, DARK, LIGHT_DARK_TOOLBAR)
                         .withActivityStyle(Libs.ActivityStyle.LIGHT)
