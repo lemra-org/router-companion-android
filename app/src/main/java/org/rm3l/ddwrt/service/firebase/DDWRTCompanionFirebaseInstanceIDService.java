90fa128711754c36a0bb732738b573e8a81aa4b3
Armel Soro <armel@rm3l.org>
2016-10-03 00:07:52 +0200

    Pave the way for an integration with Firebase Cloud Messaging (FCM)

--- app/src/main/java/org/rm3l/ddwrt/service/firebase/DDWRTCompanionFirebaseInstanceIDService.java
+++ app/src/main/java/org/rm3l/ddwrt/service/firebase/DDWRTCompanionFirebaseInstanceIDService.java
@@ -1,5 +1,6 @@
 package org.rm3l.ddwrt.service.firebase;
 
+import android.support.annotation.NonNull;
 import android.util.Log;
 
 import com.google.firebase.iid.FirebaseInstanceId;
@@ -15,17 +16,37 @@ public class DDWRTCompanionFirebaseInstanceIDService extends FirebaseInstanceIdS
 
     private static final String TAG = DDWRTCompanionFirebaseInstanceIDService.class.getSimpleName();
 
+    /**
+     * Called if InstanceID token is updated. This may occur if the security of
+     * the previous token had been compromised. Note that this is called when the InstanceID token
+     * is initially generated so this is where you would retrieve the token.
+     */
+    // [START refresh_token]
     @Override
     public void onTokenRefresh() {
         // Get updated InstanceID token.
-        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        final String refreshedToken = FirebaseInstanceId.getInstance().getToken();
         Log.d(TAG, "Refreshed token: " + refreshedToken);
 
-        //TODO After you've obtained the token, you can send it to your app server and store it using your preferred method.
+        //TODO After you've obtained the token, you can send it to your app server and
+        // store it using your preferred method.
         // If you want to send messages to this application instance or
         // manage this apps subscriptions on the server side, send the
         // Instance ID token to your app server.
-//        sendRegistrationToServer(refreshedToken);
+        sendRegistrationToServer(refreshedToken);
+    }
+    // [END refresh_token]
+
+    /**
+     * Persist token to third-party servers.
+     *
+     * Modify this method to associate the user's FCM InstanceID token with any server-side account
+     * maintained by your application.
+     *
+     * @param token The new token.
+     */
+    private void sendRegistrationToServer(final String token) {
+        // TODO: Implement this method to send token to your app server.
     }
 
 }
