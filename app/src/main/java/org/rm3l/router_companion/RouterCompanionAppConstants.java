f4003456b508d63e41412b9e9325af0f8e1d3483
Armel Soro <armel@rm3l.org>
2019-04-27 16:37:19 +0200

    Use HTTPS to communicate with Cloudinary remote service

--- app/src/main/java/org/rm3l/router_companion/RouterCompanionAppConstants.java
+++ app/src/main/java/org/rm3l/router_companion/RouterCompanionAppConstants.java
@@ -235,7 +235,7 @@ public final class RouterCompanionAppConstants {
 
     public static final String[] CLOUDINARY_OPTS = new String[]{"w_270", "h_150", "e_blur"};
 
-    public static final String IMAGE_CDN_URL_PREFIX = "http://res.cloudinary.com/rm3l/image/upload";
+    public static final String IMAGE_CDN_URL_PREFIX = "https://res.cloudinary.com/rm3l/image/upload";
 
     public static final String LAST_KNOWN_VERSION = "lastKnownVersion";
 
