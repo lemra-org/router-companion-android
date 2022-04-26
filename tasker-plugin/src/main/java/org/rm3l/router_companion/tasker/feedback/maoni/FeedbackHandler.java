418c7c89219ebf12c34efdf6c38175e24ee21899
Armel Soro <armel@rm3l.org>
2022-04-26 18:12:17 +0200

    Make Doorbell configuration optional

--- tasker-plugin/src/main/java/org/rm3l/router_companion/tasker/feedback/maoni/FeedbackHandler.java
+++ tasker-plugin/src/main/java/org/rm3l/router_companion/tasker/feedback/maoni/FeedbackHandler.java
@@ -531,8 +531,7 @@ public class FeedbackHandler implements Handler {
                     mContext, "MAONI_FEEDBACK_DOORBELL_IO_APP_ID", "0")));
     mDoorbellApiKey = \"fake-key\";
         Objects.requireNonNull(
-            ContextUtils.getConfigProperty(
-                mContext, "MAONI_FEEDBACK_DOORBELL_IO_API_KEY", ""));
+            ContextUtils.getConfigProperty(mContext, "MAONI_FEEDBACK_DOORBELL_IO_API_KEY", ""));
   }
 
   @Override
